3ef555d130a47061faf1baf23bc5ebc37626c44c
Dennis Schürholz <deschuer@informatik.uni-bremen.de>
2018-08-28 17:37:38 +0200

    Better display of Buildung and Room
    * Buildings may have a "display name" e.g. SpT = Sportturm
    * Buildung and Rooms are now seperated clearly:
      <buildung>, Raum <room>

--- app/models/stuga.rb
+++ app/models/stuga.rb
@@ -1,4 +1,16 @@
 class Stuga < ApplicationRecord
   self.table_name = 'stugen'
   belongs_to :faculty
+
+  def display_name(attr)
+    case attr
+    when :building
+      return "IW 1+2" if building == "IW"
+      return "Sportturm" if building == "SpT"
+      return "Grazer Str. 4" if building == "GRAZ"
+      building
+    else
+      self.try(attr)
+    end
+  end
 end
