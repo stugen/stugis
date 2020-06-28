5ea96d200eda5d078945524cfdd0da7a8b832712
Dennis Schürholz <schuerholz@uni-bremen.de>
2020-06-28 23:36:38 +0200

    enable mail in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV['MAIL_HOST'],
+    port:                 ENV['MAIL_PORT'],
+    user_name:            ENV['MAIL_USER'],
+    password:             ENV['MAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+  config.action_mailer.default_options = {
+    from: ENV['MAIL_FROM']
+  }
 end
