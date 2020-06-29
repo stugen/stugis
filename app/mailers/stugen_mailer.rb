f5f161cb7e33dfb052ba149021e211d98f2aad9c
Dennis Schürholz <schuerholz@uni-bremen.de>
2020-06-29 22:45:09 +0200

    change mail-cc and add mail-to to notification

--- app/mailers/stugen_mailer.rb
+++ app/mailers/stugen_mailer.rb
@@ -1,12 +1,12 @@
 class StugenMailer < ApplicationMailer
   def notification
     @stuga = params[:stuga]
-    mail(to: '', cc: 'schuerholz@uni-bremen.de', subject: "#{t('mail.subjects.notification', locale: :de)} / #{t('mail.subjects.notification', locale: :en)}")
+    mail(to: "#{@stuga.label} <#{@stuge.email}>", cc: 'schuerholz+stugis_notification@uni-bremen.de', subject: "#{t('mail.subjects.notification', locale: :de)} / #{t('mail.subjects.notification', locale: :en)}")
   end
 
   def confirmation
     @stuga = params[:stuga]
     @changed = params[:changed]
-    mail(to: '', cc: 'schuerholz@uni-bremen.de', subject: "#{t('mail.subjects.confirmation', locale: :de)} / #{t('mail.subjects.confirmation', locale: :en)}")
+    mail(to: '', cc: 'schuerholz+stugis_confirmation@uni-bremen.de', subject: "#{t('mail.subjects.confirmation', locale: :de)} / #{t('mail.subjects.confirmation', locale: :en)}")
   end
 end
